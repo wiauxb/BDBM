798ed6d5f85ff57d4857f9cf83e3586120fb6b0d
wiauxb <bastien.wiaux@student.uclouvain.be>
2022-12-22 15:41:05 +0100

    added basic programs for testing

---
+++ programs/hello_var.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	char *hello = "Hello World";
+	char *bye = "Bye Bye";
+	printf("%s\n", hello);
+	return 0;
+}
---
+++ programs/printer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("%d", 4242);
+	return 0;
+}
