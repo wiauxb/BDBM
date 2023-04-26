ffee7a3c1e29c21ea228f9a63834249e8dff0249
wiauxb <bastien.wiaux@student.uclouvain.be>
2023-04-26 11:47:59 +0200

    test multiple files

---
+++ programs/toy_multifiles/main.c
@@ -0,0 +1,9 @@
+
+
+#include "printer.h"
+
+int main(int argc, char const *argv[])
+{
+    print_things();
+    return 0;
+}
---
+++ programs/toy_multifiles/printer.c
@@ -0,0 +1,6 @@
+#include "printer.h"
+#include <stdio.h>
+
+void print_things(){
+    printf("TESTESTESTESTEST\n");
+}
