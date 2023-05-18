bf542eebcb1531fe98c7a2a8c35645fafeea185e
Arnold <arnold.gauthier@gmail.com>
2023-05-18 12:30:51 +0200

    Working random if adder

--- mutator/adder/if_adder.py
+++ mutator/adder/if_adder.py
@@ -1,55 +1,193 @@
-from helpers.adder_utils import *
-from helpers.adder_ref import TYPES, adderRef
+from .helpers.adder_utils import *
+from .helpers.adder_ref import TYPES, adderRef
 import random
-import os
 
-def generate_if(print_var, len):
+def generate_basic_if_print(print_var, len_m):
     """
+    Generate basic if that is always true
+
     print_var -- name of the variable we want to print
+    len_m -- length of the message we want to print
+    return -- condition bloc and if bloc
     """
     const = str(round(time.time()*1000))
-    if_name ="BB_" + const #faudrait rajouter un if par condition qu'on rajoute.
+    if_name ="BB_" + const 
 
     cond_bloc = f"""  %.not{const}.i.i = icmp eq i32 0 , 0 ;always true
   br i1 %.not{const}.i.i, label %{if_name}, label %next{const}
-  
 next{const}:\n"""
 
-    if_bloc =f"""\n{if_name}:
-  %cast{const}= getelementptr [{len} x i8], [{len} x i8]* {print_var}, i64 0, i64 0
+    if_bloc =f"""{if_name}:
+  %cast{const}= getelementptr [{len_m} x i8], [{len_m} x i8]* {print_var}, i64 0, i64 0
   call i32 @puts(i8* %cast{const})
   br label %next{const}
 """
     return cond_bloc, if_bloc
 
-def insert_if(project):
+def insert_basic_if_print(project, messages):
+    """Insert basic if 0=0 in the project that print the messages (1 if per message)"""
+    for message in messages:
+        string_to_print = "@.str"+str(round(time.time()*1000))
+        var_line = f"""{string_to_print} = private unnamed_addr constant [{len(message)} x i8] c\"{message}\"\n"""
 
-    string_to_print = "@.str"+str(round(time.time()*1000))
-    message = "Hello There!"
-    var_line = f"""{string_to_print} = private unnamed_addr constant [{len(message)} x i8] c\"{message}\"\n"""
+        cond, branch = generate_basic_if_print(string_to_print, len(message))
 
-    cond, branch = generate_if(string_to_print, len(message))
+        with open("s2e/projects/" + project + "/s2e-out/recovered.ll", 'r') as fp:
+            lines = fp.readlines()
 
-    with open("s2e/projects/" + project + "/s2e-out/recovered.ll", 'r') as fp:
-        lines = fp.readlines()
+        #Check if puts is already declared
+        decl_puts = False
+        for i, line in enumerate(lines):
+            if re.search(r"^@.* =", line):
+                var_line_num = i+1
+            match = re.search(r"declare .* @(\w{2,}).*\(.*", line)
+            if match != None and match[1] == "puts":
+                    decl_puts = True
 
-    for i, line in enumerate(lines):
-        if re.search(r"^@.* =", line):
-            var_line_num = i+1
-    lines.insert(var_line_num, var_line)
+        begin_main, end_main = find_main(project)
+        
 
-    begin_main, end_main = find_main(project)
+        lines.insert(end_main-1, branch)
+        insert_cond = random.randrange(begin_main+1, end_main-2)
+        while re.search(r"next\d{7,}:", lines[insert_cond]) != None:
+            insert_cond = random.randrange(begin_main+1, end_main-2)
+            
+        lines.insert(insert_cond, cond) 
+        if(decl_puts ==False):
+            lines.insert(begin_main-1, "declare dso_local i32 @puts(i8* noundef) local_unnamed_addr #3\n")
+        lines.insert(var_line_num, var_line)
+
+        with open("s2e/projects/" + project + "/s2e-out/recovered.ll", 'w') as fp:
+           fp.writelines(lines)
+
+
+
+
+def generate_random_func_decl(lines):
+    """
+    Generate the declaration of functions that will be needed
     
-    lines.insert(random.randrange(begin_main+2, end_main-3), cond) #TODO : pas sur pour le -2
+    lines -- lines from project
+    return -- function delcaration that needs to be added before main
+    """
+    
+    decl_rand =False    
+    decl_srand = False
+    decl_time = False
+    for line in lines:
+        match = re.search(r"declare .* @(\w{2,}).*\(.*", line)
+        if match != None and match[1] == "rand":
+            decl_rand = True
+        elif match != None and match[1] == "srand":
+            decl_srand = True
+        elif match != None and match[1] == "time":
+            decl_time = True
+    
+    func_decl = ""
+    if(decl_rand == False):
+        func_decl += "declare i32 @rand() local_unnamed_addr  noinline\n\n" 
+    if(decl_srand == False):
+        func_decl += "declare void @srand(i32) local_unnamed_addr  noinline \n\n"
+    if(decl_time == False):
+        func_decl += "declare i32 @time(i32*) local_unnamed_addr noinline \n\n"
+    return func_decl
 
-    print(end_main)
-    lines.insert(end_main+1, branch)
 
-    with open("s2e/projects/" + project + "/s2e-out/recovered.ll", 'w') as fp:
-        fp.writelines(lines)
+def generate_random_main(lines, max_rand, iteration):
+    """
+    Generate lines that store a random number with max mavule max_value
+
+    lines -- lines in the project
+    max_rand -- max value for the random number
+    iteration -- iteration of the adder
+    return -- declaration of the random number, name of the variable that stocks the random number
+    """
+
+    time_var = "%time" + str(round(time.time()*1000)+iteration)
+
+    srand_main = False
+    for line in lines : 
+        if re.search(r"tail call void @srand.*", line) !=None:
+            srand_main =True
+
+    main_decl = f""""""
+    if srand_main == False:
+        main_decl += f"""  {time_var} = tail call i32 @time(i32* null)
+  tail call void @srand(i32 {time_var})\n"""
+    rand_init = "%rand_init" + str(round(time.time()*1000))
+    main_decl += f"""  {rand_init} = tail call i32 @rand()\n"""
+    rand_fin = "%rand_fin" + str(round(time.time()*1000))
+    main_decl += f"""  {rand_fin} = srem i32 {rand_init}, {max_rand}\n"""
+
+    return main_decl, rand_fin
+
+def generate_random_if(max_rand, rand_var, iteration):
+    """
+    Generate the random if condition and the return bloc.
+
+    max_rand -- max random possible
+    rand_var -- variable in which the first random is stored
+    iteration -- iteration of the adder
+    return -- if condition and the return bloc
+    """
+    cond = str(random.randrange(max_rand))
+    const = str(round(time.time()*1000) + iteration)
+    if_name ="BB_" + const 
+
+    cond_bloc = f"""  %.not{const}.i.i = icmp eq i32 {cond} , {rand_var} 
+  br i1 %.not{const}.i.i, label %next{const}, label %{if_name}
+next{const}:\n"""
+
+    if_bloc =f"""{if_name}:
+  ret void
+"""
+    return cond_bloc, if_bloc
+
+def add_random_in_main(project, max_rand, iterations):
+    """
+    add a condition that checks if two random numbers are equal. If they are not, return.
+
+    project -- name of the project
+    max_rand -- max random possible
+    iterations -- number of ifs we want to add in project
+
+    """
+
+    for iteration in range(iterations) :
+        #The program is too fast, so we need to add iteration to every time we need
+        with open("s2e/projects/" + project + "/s2e-out/recovered.ll", 'r') as fp:
+            lines = fp.readlines()
+        
+        fun_decl = generate_random_func_decl(lines)
+        main_decl, rand_fin = generate_random_main(lines, max_rand, iteration)
+        cond_bloc, if_bloc = generate_random_if(max_rand, rand_fin, iteration)
+
+        begin_main, end_main = find_main(project)
+
+        #makes sure that not two next follow each other TODO : confirm it is correct
+        insert_cond = random.randrange(begin_main+1, end_main-2)
+        while re.search(r"next\d{7,}:", lines[insert_cond]) != None:
+            insert_cond = random.randrange(begin_main+1, end_main-2)
+
+
+        lines.insert(end_main-1, if_bloc)
+        insert_cond = random.randrange(begin_main+1, end_main-2)
+        while re.search(r"next\d{7,}:", lines[insert_cond]) != None:
+            insert_cond = random.randrange(begin_main+1, end_main-2)
+            
+        lines.insert(insert_cond, cond_bloc)
+
+        lines.insert(begin_main+1, main_decl)
+        
+        lines.insert(begin_main-2, fun_decl)
+
+        with open("s2e/projects/" + project + "/s2e-out/recovered.ll", 'w') as fp:
+            fp.writelines(lines)
+
+    
 
 
 if __name__ == "__main__":
-    insert_if("hello")
-
-
+    #insert_basic_if_print("hello", ["Premier\0", "Deuxieme\0"])
+    
+    add_random_in_main("hello", 3, 2)
--- mutator/auto_mutation.py
+++ mutator/auto_mutation.py
@@ -1,7 +1,7 @@
 from .sleeper import sleeper
 from .strings import split, xor
 import argparse
-from .adder import code_adder,cleanware_adder
+from .adder import code_adder,cleanware_adder,if_adder
 
 
 
@@ -10,16 +10,23 @@ if __name__ == "__main__": #FIXME think about ordering the mutations
     main_parser = argparse.ArgumentParser()
     main_parser.add_argument("project_name")
     subparsers = main_parser.add_subparsers(help="Operation", dest='command')
+    
     string_parser = subparsers.add_parser("strings")
     string_parser.add_argument("kind", choices=["split", "xor", "base64"])
     string_parser.add_argument("--text", action='store_true')
     string_parser.add_argument("-p", "--probability", help="Probability of mutation", default=1.0)
     string_parser.add_argument("-n", "--number", help="Number of mutations to generate", default=1)
+    
     clean_parser = subparsers.add_parser("sleep")
     clean_parser = subparsers.add_parser("clean_adder")
-    clean_parser.add_argument("number_add", help="Number of calls to add", default=1)
+    clean_parser.add_argument("--number_add", help="Number of calls to add", default=1)
+    
     sys_parser = subparsers.add_parser("sys_adder")
-    sys_parser.add_argument("number_add", help="Number of calls to add", default=1)
+    sys_parser.add_argument("--number_add", help="Number of calls to add", default=1)
+
+    rand_if_parser = subparsers.add_parser("random_if")
+    rand_if_parser.add_argument("--max_random", help ="Maximum value for the if comparison", default = 5)
+    rand_if_parser.add_argument("--iterations", help = "Number of if's to add in the project", default = 1)
 
     args = main_parser.parse_args()
  
@@ -43,3 +50,6 @@ if __name__ == "__main__": #FIXME think about ordering the mutations
     elif args.command == "sys_adder":
         code_adder.clone_recovered(project)
         lines_added = code_adder.insert_sys_calls(int(args.number_add), project)
+    elif args.command == "random_if":
+        code_adder.clone_recovered(project)
+        if_adder.add_random_in_main(project, int(args.max_random), int(args.iterations))
