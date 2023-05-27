07aaa7221126b6069891c9a0e56e2c63800de0a6
Arnold <arnold.gauthier@gmail.com>
2023-05-27 17:47:29 +0200

    Little modif

--- mutator/mutation_generation.py
+++ mutator/mutation_generation.py
@@ -18,8 +18,8 @@ def mutation_selection(project, num_of_mutations):
     subprocess.check_output(command, shell=True, timeout = 30)
     if not os.path.exists("s2e/projects/"+project+"/mutations"):
         os.makedirs("s2e/projects/"+project+"/mutations")
-    a_raj = ["strings_split" ]
-    mutations = ["escape_envvar", "string_xor" ,"clean_adder", "random_if", "replace_puts", "sys_adder", "basic_if", "escape_traced", "escape_vm"]
+    a_raj = []
+    mutations = ["clean_adder", "escape_envvar", "string_xor" , "random_if", "replace_puts", "sys_adder", "basic_if", "escape_traced", "escape_vm", "strings_split"]
     nombre_mutations = random.randrange(1, len(mutations)+1)
     mutations_to_do = []
     for i in range(nombre_mutations):
@@ -27,16 +27,17 @@ def mutation_selection(project, num_of_mutations):
         while mutation_to_append in mutations_to_do:
             mutation_to_append = mutations[random.randrange(0, len(mutations))]
         mutations_to_do.append(mutation_to_append)
-
+    #mutations_to_do = ["clean_adder", "strings_split"]
+    #mutations_to_do = [mutations[num_of_mutations-1]]
     just_com = "mutations : " + str(num_of_mutations) + "\n"
     for mutation in mutations_to_do : 
         if mutation == "strings_split":
             just_com += mutate_strings(project, "split")
         if mutation == "string_base64":
             just_com += mutate_strings(project, "base64")
-        if mutation == "strings_split":
+        if mutation == "strings_xor":
             just_com += mutate_strings(project, "xor")
-        if mutation == "string_xor":
+        if mutation == "clean_adder":
             just_com += mutate_clean_adder(project)
         if mutation == "random_if":
             just_com += mutate_random_if(project)
@@ -69,28 +70,30 @@ def mutation_selection(project, num_of_mutations):
         else:
             command = "just recompile " + project
         subprocess.check_output(command, shell=True, timeout = 30)
-
-        shutil.copy("s2e/projects/" +project +"/s2e-out/recovered.ll", "s2e/projects/"+project+"/mutations/mutations"+str(num_of_mutations)+".ll")
-        shutil.copy("s2e/projects/" +project +"/s2e-out/custom_recovered", "s2e/projects/"+project+"/mutations/mutations"+str(num_of_mutations)+"_exec")
-
-        try:
-            with open("s2e/projects/"+project+"/mutations/mutations_record.txt", "r") as f:
-                lines = f.readlines()
-            lines.append(just_com)
-        except Exception as error:
-            print(error)
-            lines = just_com
-        
-        with open("s2e/projects/"+project+"/mutations/mutations_record.txt", "w") as f:
-            f.writelines(lines)
-        if(num_of_mutations <=1):
-            return
-        else:
-            mutation_selection(project, num_of_mutations-1)
+    
     except Exception as error:
         print("couldn't recompile mutation, starting again")
         exit()
         mutation_selection(project, num_of_mutations)
+
+    shutil.copy("s2e/projects/" +project +"/s2e-out/recovered.ll", "s2e/projects/"+project+"/mutations/mutations"+str(num_of_mutations)+".ll")
+    shutil.copy("s2e/projects/" +project +"/s2e-out/custom_recovered", "s2e/projects/"+project+"/mutations/mutations"+str(num_of_mutations)+"_exec")
+
+    try:
+        with open("s2e/projects/"+project+"/mutations/mutations_record.txt", "r") as f:
+            lines = f.readlines()
+        lines.append(just_com)
+    except Exception as error:
+        print(error)
+        lines = just_com
+    
+    with open("s2e/projects/"+project+"/mutations/mutations_record.txt", "w") as f:
+        f.writelines(lines)
+    if(num_of_mutations <=1):
+        return
+    else:
+        mutation_selection(project, num_of_mutations-1)
+   
 
 def mutate_basic_if(project):
     words_liste = ["Premier ", "Deuxième ", "troisieme ", "okiojvi,er ", "dergbjzeka ", "zefiĵht "]
@@ -131,7 +134,7 @@ def mutate_random_if(project):
 
 def mutate_clean_adder(project):
     number = random.randrange(1,11)
-    command = f"""just mutate {project} clean_adder --number {number}"""
+    command = f"""just mutate {project} clean_adder --number_add {number}"""
     subprocess.check_output(command, shell=True)
     return command
 
@@ -142,9 +145,10 @@ def mutate_strings(project, choice):
     if choice == "split":
         ncuts = random.randrange(2, 15) #15 arbitrairy, we can choose whatever we want
     command = f"""just mutate {project} strings {choice} --ncuts {ncuts}"""
-    #if text:
-    #    command += " --text"
-    subprocess.check_output(command, shell=True)
+    if text==0:
+        command += " --text"
+    output = subprocess.check_output(command, shell=True)
+    print(output.decode())
     return command
 
     
