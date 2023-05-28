6b696476929d88d11af9b5ad6e938ca1eafe9d1d
wiauxb <bastien.wiaux@student.uclouvain.be>
2023-05-28 20:26:51 +0200

    added graph_extractor

--- graph_extractor.sh
+++ graph_extractor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PWD=$(pwd)
-mkdir -p graphs/png
-mkdir -p graphs/pdf
-
-for f in $(find . -name *.gv); do
-  echo $f
-  timeout 120 dot $f -Tpng -O
-  timeout 120 dot $f -Tpdf -O
-  mv $f.png graphs/png
-  mv $f.pdf graphs/pdf
+for dir in $(find . -mindepth 1 -maxdepth 1 -type d); do
+  mkdir -p graphs/$dir/png
+  mkdir -p graphs/$dir/pdf
+  for f in $(find $dir -name *.gv); do
+    echo $f
+    timeout 120 dot $f -Tpng -O 
+    timeout 120 dot $f -Tpdf -O
+    mv $f.png graphs/$dir/png
+    mv $f.pdf graphs/$dir/pdf
+  done
 done
